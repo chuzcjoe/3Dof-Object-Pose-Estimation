df9ecaddcd8d158c1b35bb81610db4b348897f26
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-01-30 20:27:11 -0500

    add two vectors

--- test_on_img.py
+++ test_on_img.py
@@ -16,25 +16,33 @@ import matplotlib.pyplot as plt
 
 class Test:
 
-    def __init__(self,model,snapshot,num_classes):
+    def __init__(self,model1,model2,snapshot1,snapshot2,num_classes):
         
         self.num_classes = num_classes
-        self.model = model(num_classes=self.num_classes)
-        self.saved_state_dict = torch.load(snapshot)
-        self.model.load_state_dict(self.saved_state_dict)
-        self.model.cuda(0)
+        self.model1 = model1(num_classes=self.num_classes)
+        self.model2 = model2(num_classes=self.num_classes)
 
-        self.model.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+        self.saved_state_dict1 = torch.load(snapshot1)
+        self.model1.load_state_dict(self.saved_state_dict1)
+        self.model1.cuda(0)
+
+        self.saved_state_dict2 = torch.load(snapshot2)
+        self.model2.load_state_dict(self.saved_state_dict2)
+        self.model2.cuda(0)
+
+
+        self.model1.eval()  # Change model to 'eval' mode (BN uses moving mean/var).
+        self.model2.eval()
 
         self.softmax = nn.Softmax(dim=1).cuda(0)
 
-    def draw_attention_vector(self, pred_vector, img):
+    def draw_attention_vector(self, pred_vector1, pred_vector2, img):
         #save_dir = os.path.join(args.save_dir, 'show_front')
         #img_name = os.path.basename(img_path)
 
         #img = cv.imread(img_path)
 
-        predx, predy, predz = pred_vector
+        predx, predy, predz = pred_vector1
         #img = np.squeeze(img,axis=0)
         #print(img.shape)
         #img = img.reshape((224,224,3))
@@ -42,6 +50,10 @@ class Test:
 
         # draw pred attention vector with red
         utils.draw_front(img, predy, predz, tdx=None, tdy=None, size=100, color=(0, 0, 255))
+
+
+        predx, predy, predz = pred_vector2
+        utils.draw_front(img, predy, predz, tdx=None, tdy=None, size=100, color=(0, 255, 0))
 
         #cv.imwrite(os.path.join(save_dir, img_name), img)
         cv.imshow("test_result",img)
@@ -54,12 +66,20 @@ class Test:
             #print(images.shape)
 
             # get x,y,z cls predictions
-            x_cls_pred, y_cls_pred, z_cls_pred = self.model(images)
+            x_cls_pred, y_cls_pred, z_cls_pred = self.model1(images)
 
             # get prediction vector(get continue value from classify result)
-            _, _, _, pred_vector = utils.classify2vector(x_cls_pred, y_cls_pred, z_cls_pred, self.softmax, self.num_classes)
+            _, _, _, pred_vector1 = utils.classify2vector(x_cls_pred, y_cls_pred, z_cls_pred, self.softmax, self.num_classes)
 
-            self.draw_attention_vector(pred_vector[0].cpu().tolist(),
+
+
+            # get x,y,z cls predictions
+            x_cls_pred, y_cls_pred, z_cls_pred = self.model2(images)
+
+            # get prediction vector(get continue value from classify result)
+            _, _, _, pred_vector2 = utils.classify2vector(x_cls_pred, y_cls_pred, z_cls_pred, self.softmax, self.num_classes)
+
+            self.draw_attention_vector(pred_vector1[0].cpu().tolist(), pred_vector2[0].cpu().tolist(),
                                           draw_img)
 
 #input_size = 224
