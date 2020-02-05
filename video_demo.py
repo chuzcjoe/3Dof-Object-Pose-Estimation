fc05a6278a03d9ec94698509d7c4930b3b9a94b5
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-02-05 18:33:18 -0500

    Update video_demo.py

--- video_demo.py
+++ video_demo.py
@@ -5,6 +5,7 @@ from net import MobileNetV2
 from torchvision import transforms
 from net import MobileNetV2
 import argparse
+import matplotlib.pyplot as plt
 
 def parse_args():
     """Parse input arguments."""
@@ -32,8 +33,9 @@ if __name__ == '__main__':
     test = Test(MobileNetV2, MobileNetV2, args.snapshot1, args.snapshot2, 66)
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
 
-    # make 3d axes
+    # make 3D axis
     fig = plt.figure()
+    ax  = plt.axes(projection='3d')
 
     while 1:
         _, frame = cap.read()
