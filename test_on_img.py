1137fad8cce58b6570cdb37a5d04d9c93a1e4b6d
Joe <44826941+chuzcjoe@users.noreply.github.com>
2020-02-05 17:03:05 -0500

    Update test_on_img.py

--- test_on_img.py
+++ test_on_img.py
@@ -36,7 +36,7 @@ class Test:
 
         self.softmax = nn.Softmax(dim=1).cuda(0)
 
-    def draw_attention_vector(self, pred_vector1, pred_vector2, img):
+    def draw_attention_vector(self, pred_vector1, pred_vector2, img, ax):
         #save_dir = os.path.join(args.save_dir, 'show_front')
         #img_name = os.path.basename(img_path)
 
@@ -54,13 +54,14 @@ class Test:
 
         predx, predy, predz = pred_vector2
         utils.draw_front(img, predy, predz, tdx=None, tdy=None, size=100, color=(0, 255, 0))
-
+        
         #cv.imwrite(os.path.join(save_dir, img_name), img)
         cv.imshow("test_result",img)
+        utils.draw_3d_coor(pred_vector1, pred_vector2, ax)
         #plt.imshow(img)
         #plt.show()
 
-    def test_per_img(self,cv_img,draw_img):
+    def test_per_img(self,cv_img,draw_img, ax):
         with torch.no_grad():
             images = cv_img.cuda(0)
             #print(images.shape)
@@ -80,7 +81,7 @@ class Test:
             _, _, _, pred_vector2 = utils.classify2vector(x_cls_pred, y_cls_pred, z_cls_pred, self.softmax, self.num_classes)
 
             self.draw_attention_vector(pred_vector1[0].cpu().tolist(), pred_vector2[0].cpu().tolist(),
-                                          draw_img)
+                                          draw_img,ax)
 
 #input_size = 224
 #test = Test(MobileNetV2,"./results/MobileNetV2_1.0_classes_66_input_224/snapshot/MobileNetV2_1.0_classes_66_input_224_epoch_50.pkl",66)
